89760a6ed6abe75d593edd5837abd76abc49464b
Evgeny Sychev <evgeny.sychev@harman.com>
2020-08-19 17:14:17 +0300

    Initial read and parse xml from samples

--- app/src/main/java/com/sychev/rss_reader/rss_reader/DataBaseAccessor.java
+++ app/src/main/java/com/sychev/rss_reader/rss_reader/DataBaseAccessor.java
@@ -8,48 +8,48 @@ import java.sql.Array;
 import java.util.List;
 import java.util.Map;
 
+//
+//public class NewsReaderDBContract {
+//    private  NewsReaderDBContract() {}
+//
+//    public static class NewsSourceDBEntry implements BaseColumns {
+//        public static final String TABLE_NAME_SOURCE = "NEWS_SOURCES_TABLE";
+//        public static final String COLUMN_NAME_SOURCE = "NEWS_SOURCE_TITLE";
+//        public static final String COLUMN_URI_SOURCE = "NEWS_SOURCE_URI";
+//        public static final String COLUMN_LDATE_SOURCE = "NEWS_SOURCE_LASTDATE";
+//    }
+//
+//    public static class NewsDBEntry implements BaseColumns {
+//        public static final String TABLE_NAME_NEWS = "NEWS_ITEMS_TABLE";
+//        public static final String COLUMN_NAME_NEWS_TITLE = "NEWS_TITLE";
+//
+//    }
+//}
 
-public class NewsReaderDBContract {
-    private  NewsReaderDBContract() {}
-
-    public static class NewsSourceDBEntry implements BaseColumns {
-        public static final String TABLE_NAME_SOURCE = "NEWS_SOURCES_TABLE";
-        public static final String COLUMN_NAME_SOURCE = "NEWS_SOURCE_TITLE";
-        public static final String COLUMN_URI_SOURCE = "NEWS_SOURCE_URI";
-        public static final String COLUMN_LDATE_SOURCE = "NEWS_SOURCE_LASTDATE";
-    }
-
-    public static class NewsDBEntry implements BaseColumns {
-        public static final String TABLE_NAME_NEWS = "NEWS_ITEMS_TABLE";
-        public static final String COLUMN_NAME_NEWS_TITLE = "NEWS_TITLE";
-
-    }
-}
-
-public class NewsDbHelper extends SQLiteOpenHelper
-{
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
-                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
-                    FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
-                    FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
-
-    private static final String SQL_DELETE_ENTRIES =
-            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
-
-}
-
-public class DataBaseAccessor {
-    private static String TABLE_NAME = "";
-    SQLiteDatabase database;
-    public void InsertUriList(List<String> stringList)
-    {
-        TABLE_NAME = "UriNewsList";
-        String INSERT_QUERY = "insert into " + TABLE_NAME + " (ID,URI) values (?, ?)";
-        try {
-
-        }
-
-    }
-
-}
+//public class NewsDbHelper extends SQLiteOpenHelper
+//{
+//    private static final String SQL_CREATE_ENTRIES =
+//            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+//                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+//                    FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+//                    FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+//
+//    private static final String SQL_DELETE_ENTRIES =
+//            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+//
+//}
+//
+//public class DataBaseAccessor {
+//    private static String TABLE_NAME = "";
+//    SQLiteDatabase database;
+//    public void InsertUriList(List<String> stringList)
+//    {
+////        TABLE_NAME = "UriNewsList";
+////        String INSERT_QUERY = "insert into " + TABLE_NAME + " (ID,URI) values (?, ?)";
+////        try {
+////
+////        }
+//
+//    }
+//
+//}
--- app/src/main/java/com/sychev/rss_reader/rss_reader/MainActivity.java
+++ app/src/main/java/com/sychev/rss_reader/rss_reader/MainActivity.java
@@ -1,14 +1,37 @@
 package com.sychev.rss_reader.rss_reader;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.xml.parsers.DocumentBuilder;
+
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +39,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<NewsModelItem> list = new ArrayList<>();
-        list.add(new NewsModelItem("Title 1", "Decr 1"));
-        list.add(new NewsModelItem("Title 2", "Decr 2"));
-        ListView lvMain = findViewById(R.id.lvMain);
 
         // создаем адаптер
-        NewsAdapter adapter = new NewsAdapter(this, list);
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
-        // присваиваем адаптер списку
-        lvMain.setAdapter(adapter);
+        loadData();
     }
+
+    protected void loadData() {
+        try {
+            //set the download URL, a url that points to a file on the internet
+            //this is the file to be downloaded
+            URL url = new URL("https://www.gazeta.ru/export/rss/first.xml");
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(url.openStream()));
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("item");
+            System.out.printf("Readed %d elements", nodeList.getLength());
+
+            ArrayList<NewsModelItem> list = new ArrayList<>();
+            ListView lvMain = findViewById(R.id.lvMain);
+
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+
+                Node node = nodeList.item(i);
+
+                Element fstElmnt = (Element) node;
+                NodeList nameList = fstElmnt.getElementsByTagName("title");
+                Element nameElement = (Element) nameList.item(0);
+                nameList = nameElement.getChildNodes();
+
+                NodeList descrList = fstElmnt.getElementsByTagName("description");
+                Element descrListElement = (Element) descrList.item(0);
+                descrList = descrListElement.getChildNodes();
+                System.out.printf("Website = %s", ((Node) descrList.item(0)).getNodeValue());
+                list.add(new NewsModelItem(((Node) nameList.item(0)).getNodeValue(), ((Node) descrList.item(0)).getNodeValue()));
+
+
+            }
+            NewsAdapter adapter = new NewsAdapter(this, list);
+            lvMain.setAdapter(adapter);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
