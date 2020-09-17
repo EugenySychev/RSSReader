645cba31697704a3cfad93fa6f77ee51ea7c7552
Eugeny Sychev <eugeny.sychev@gmail.com>
2020-09-17 18:44:47 +0300

    interim commit

--- app/src/main/java/com/sychev/rss_reader/SourceListActivity.java
+++ app/src/main/java/com/sychev/rss_reader/SourceListActivity.java
@@ -1,9 +1,13 @@
 package com.sychev.rss_reader;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
 
@@ -26,8 +30,24 @@ public class SourceListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getBaseContext());
+                final LayoutInflater inflater = getLayoutInflater();
+                builder.setView(inflater.inflate(R.layout.source_dialog, null))
+                        .setPositiveButton(R.string.save_button_title, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                SourceModelItem item = new SourceModelItem();
+                                EditText editText = (EditText) inflater.inflate(R.id.enter_source_url_edit_text, null);
+                                System.out.println(editText.getText());
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                builder.create();
             }
         });
 
