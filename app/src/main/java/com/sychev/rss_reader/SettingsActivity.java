0f633784e0689a00230922f75f470a81fa101260
Evgeny Sychev <evgeny.sychev@harman.com>
2020-10-08 16:00:42 +0300

    Set storing setting

--- app/src/main/java/com/sychev/rss_reader/SettingsActivity.java
+++ app/src/main/java/com/sychev/rss_reader/SettingsActivity.java
@@ -1,35 +1,57 @@
 package com.sychev.rss_reader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
 
+import java.util.Objects;
 import java.util.prefs.Preferences;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final String APP_PREFERENCES = "Settings";
     private boolean useDarkTheme;
-
+    private SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        preferences = getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.settings_activity_toolbar);
         toolbar.setTitle(R.string.action_settings);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         loadReference();
+
+        final SwitchCompat switchCompat = findViewById(R.id.dark_theme_switcher);
+        switchCompat.setChecked(useDarkTheme);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                useDarkTheme = switchCompat.isChecked();
+                updateReference();
+            }
+        });
+    }
+
+    private void updateReference() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("DarkTheme", useDarkTheme);
+        editor.commit();
+
     }
 
     private void loadReference() {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        preferences.getBoolean("DarkTheme", useDarkTheme);
+        useDarkTheme = preferences.getBoolean("DarkTheme", false);
     }
 
     @Override
@@ -42,4 +64,6 @@ public class SettingsActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
 }
